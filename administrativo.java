af7a6951d36062ae324f6e7f54ae948a2ca3a5b8
mateojavierfuentesmendoza <mateojavierfuentesmendoza@gmail.com>
2025-10-17 13:26:13 -0500

    se agrega admin

--- administrativo.java
+++ administrativo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class administrativo {
+public class administrativo{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<String> citas = new ArrayList<>();
